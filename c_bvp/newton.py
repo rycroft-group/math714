48b373e95868779d0c678e790a49806ec8be6e4b
Yue Sun <yuesun@g.harvard.edu>
2025-09-08 21:05:53 -0500

    Reformat code example.

--- c_bvp/newton.py
+++ c_bvp/newton.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-from math import sin,cos
+from math import sin, cos
 
 # Function to perform root-finding on
 def f(x):
@@ -9,13 +9,14 @@ def f(x):
 def df(x):
     return cos(x)+3*x*x
 
+
 # Choose starting point for Newton iteration
-xa=1
+xa = 1
 
 for k in range(20):
 
     # Print out the current iterate, and the function value there
-    print("%17.10g %17.10g" % (xa,f(xa)))
+    print("%17.10g %17.10g" % (xa, f(xa)))
 
     # Perform Newton step
-    xa=xa-f(xa)/df(xa)
+    xa = xa-f(xa)/df(xa)
